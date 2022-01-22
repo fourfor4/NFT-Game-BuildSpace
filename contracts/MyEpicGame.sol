ab57763a93536de3d7c802a91aa46adbfdb568e1
gru <ryanstevens.rs1125@gmail.com>
2022-01-22 13:57:41 -0800

    init sol

---
+++ contracts/MyEpicGame.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract MyEpicGame {
+    constructor() {
+        console.log("THIS IS MY GAME CONTRACT, NICE.");
+    }
+}
---
+++ scripts/run.js
@@ -0,0 +1,18 @@
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+  const gameContract = await gameContractFactory.deploy();
+  await gameContract.deployed();
+  console.log("Contract deployed to:", gameContract.address);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+runMain();
