adc2e9e06b6c8be2a4beaf0e1f20b3c239f4ef5b
gru <ryanstevens.rs1125@gmail.com>
2022-01-22 20:47:40 -0800

    upgrade sol with mint function

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -1,10 +1,91 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+// NFT contract to inherit from.
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+// Helper function OpenZepplin provides.
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 import "hardhat/console.sol";
 
 contract MyEpicGame {
-    constructor() {
-        console.log("THIS IS MY GAME CONTRACT, NICE.");
+    // We'll hold our character's attributes in a struct. Feel free to add
+    // whatever you'd like as an attribute! (ex. defense, crit chance, etc).
+    struct CharacterAttributes {
+        uint256 characterIndex;
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+
+    using Counters for Counters.Counter;
+    Counters.counter private _tokenIds;
+
+    CharacterAttributes[] defaultCharaters;
+
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+    mapping(address => uint256) public nftHolders;
+
+    // Data passed in to the contract when it's first created initializing the CharacterAttributes.
+    // We're going to actually pass these values in from run.js.
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint256[] memory characterHp,
+        uint256[] memory characterAttackDmg
+    ) ERC721("Heroes", "HERO") {
+        // Loop through all the characters, and save their values in our contract so
+        // we can use them later when we mint our NFTs.
+        for (uint256 i = 0; i < characterNames.length; i++) {
+            defaultCharaters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURIs[i],
+                    hp: characterHp[i],
+                    maxHp: characterHp[i],
+                    attackDamage: characterAttackDmg[i]
+                })
+            );
+
+            CharacterAttributes memory c = defaultCharaters[i];
+            console.log(
+                "Done initializeing %s w/ HP %s, img %s",
+                c.name,
+                c.hp,
+                c.imageURI
+            );
+        }
+
+        _tokenIds.increment();
+    }
+
+    function mintCharacterNFT(uint256 _characterIndex) external {
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(msg.sender, newItemId);
+
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            imageURI: defaultCharacters[_characterIndex].imageURI,
+            hp: defaultCharacters[_characterIndex].hp,
+            maxHp: defaultCharacters[_cahracterIndex].maxHp,
+            attactDamage: defaultCharacters[_characterIndex].attackDamage
+        });
+
+        console.log(
+            "Minted NFT w/ tokenId %s and CharacterIndex %s",
+            newItemId,
+            _characterIndex
+        );
+
+        nftHolders[msg.sender] = newItemId;
+        _tokenIds.increment();
     }
 }
