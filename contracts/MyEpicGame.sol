c6b15143e6a962668aadd13e03c5054fdeee278f
gru <ryanstevens.rs1125@gmail.com>
2022-01-22 22:04:25 -0800

    add base64 sol

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -2,16 +2,15 @@
 pragma solidity ^0.8.0;
 
 // NFT contract to inherit from.
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-// Helper function OpenZepplin provides.
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
 import "hardhat/console.sol";
 
-contract MyEpicGame {
+import "./libraries/Base64.sol";
+
+contract MyEpicGame is ERC721 {
     // We'll hold our character's attributes in a struct. Feel free to add
     // whatever you'd like as an attribute! (ex. defense, crit chance, etc).
     struct CharacterAttributes {
@@ -24,9 +23,9 @@ contract MyEpicGame {
     }
 
     using Counters for Counters.Counter;
-    Counters.counter private _tokenIds;
+    Counters.Counter private _tokenIds;
 
-    CharacterAttributes[] defaultCharaters;
+    CharacterAttributes[] defaultCharacters;
 
     mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
 
@@ -43,7 +42,7 @@ contract MyEpicGame {
         // Loop through all the characters, and save their values in our contract so
         // we can use them later when we mint our NFTs.
         for (uint256 i = 0; i < characterNames.length; i++) {
-            defaultCharaters.push(
+            defaultCharacters.push(
                 CharacterAttributes({
                     characterIndex: i,
                     name: characterNames[i],
@@ -54,7 +53,7 @@ contract MyEpicGame {
                 })
             );
 
-            CharacterAttributes memory c = defaultCharaters[i];
+            CharacterAttributes memory c = defaultCharacters[i];
             console.log(
                 "Done initializeing %s w/ HP %s, img %s",
                 c.name,
@@ -75,8 +74,8 @@ contract MyEpicGame {
             name: defaultCharacters[_characterIndex].name,
             imageURI: defaultCharacters[_characterIndex].imageURI,
             hp: defaultCharacters[_characterIndex].hp,
-            maxHp: defaultCharacters[_cahracterIndex].maxHp,
-            attactDamage: defaultCharacters[_characterIndex].attackDamage
+            maxHp: defaultCharacters[_characterIndex].maxHp,
+            attackDamage: defaultCharacters[_characterIndex].attackDamage
         });
 
         console.log(
@@ -87,5 +86,46 @@ contract MyEpicGame {
 
         nftHolders[msg.sender] = newItemId;
         _tokenIds.increment();
+    }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[
+            _tokenId
+        ];
+
+        string memory strHp = Strings.toString(charAttributes.hp);
+        string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+        string memory strAttackDamage = Strings.toString(
+            charAttributes.attackDamage
+        );
+
+        string memory json = Base64.encode(
+            abi.encodePacked(
+                '{"name": "',
+                charAttributes.name,
+                " -- NFT #: ",
+                Strings.toString(_tokenId),
+                '", "description": "This is an NFT that lets people play in the game Metaverse Slayer!", "image": "',
+                charAttributes.imageURI,
+                '", "attributes": [ { "trait_type": "Health Points", "value": ',
+                strHp,
+                ', "max_value":',
+                strMaxHp,
+                '}, { "trait_type": "Attack Damage", "value": ',
+                strAttackDamage,
+                "} ]}"
+            )
+        );
+
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64", json)
+        );
+
+        return output;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -10,6 +10,14 @@ const main = async () => {
   );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
+
+  let txn;
+
+  txn = await gameContract.mintCharacterNFT(2);
+  await txn.wait();
+
+  let returnedTokenUri = await gameContract.tokenURI(1);
+  console.log("Token URI:", returnedTokenUri);
 }
 
 const runMain = async () => {
