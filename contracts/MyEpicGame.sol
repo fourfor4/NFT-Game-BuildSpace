eed58eee5e17af52626411466bd0649a47ffc13b
gru <ryanstevens.rs1125@gmail.com>
2022-01-24 21:26:12 -0800

    add dapp

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -22,6 +22,16 @@ contract MyEpicGame is ERC721 {
         uint256 attackDamage;
     }
 
+    struct BigBoss {
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+
+    BigBoss public bigBoss;
+
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -31,14 +41,39 @@ contract MyEpicGame is ERC721 {
 
     mapping(address => uint256) public nftHolders;
 
+    event CharacterNFTMinted(
+        address sender,
+        uint256 tokenId,
+        uint256 characterIndex
+    );
+    event AttackComplete(uint256 newBossHp, uint256 newPlayerHp);
+
     // Data passed in to the contract when it's first created initializing the CharacterAttributes.
     // We're going to actually pass these values in from run.js.
     constructor(
         string[] memory characterNames,
         string[] memory characterImageURIs,
         uint256[] memory characterHp,
-        uint256[] memory characterAttackDmg
+        uint256[] memory characterAttackDmg,
+        string memory bossName,
+        string memory bossImageURI,
+        uint256 bossHp,
+        uint256 bossAttackDamage
     ) ERC721("Heroes", "HERO") {
+        bigBoss = BigBoss({
+            name: bossName,
+            imageURI: bossImageURI,
+            hp: bossHp,
+            maxHp: bossHp,
+            attackDamage: bossAttackDamage
+        });
+
+        console.log(
+            "Done initializing boss %s w/ HP %s, img %s",
+            bigBoss.name,
+            bigBoss.hp,
+            bigBoss.imageURI
+        );
         // Loop through all the characters, and save their values in our contract so
         // we can use them later when we mint our NFTs.
         for (uint256 i = 0; i < characterNames.length; i++) {
@@ -85,6 +120,8 @@ contract MyEpicGame is ERC721 {
         );
 
         nftHolders[msg.sender] = newItemId;
+        emit CharacterNFTMinted(msg.sender, newItemId, _characterIndex);
+
         _tokenIds.increment();
     }
 
@@ -123,9 +160,85 @@ contract MyEpicGame is ERC721 {
         );
 
         string memory output = string(
-            abi.encodePacked("data:application/json;base64", json)
+            abi.encodePacked("data:application/json;base64,", json)
         );
 
         return output;
+    }
+
+    function attackBoss() public {
+        // Get the state of the player's NFT.
+        uint256 nftTokenIdOfPlayer = nftHolders[msg.sender];
+        CharacterAttributes storage player = nftHolderAttributes[
+            nftTokenIdOfPlayer
+        ];
+
+        console.log(
+            "\nPlayer w/ character %s about to attack. Has %s HP and %s AD",
+            player.name,
+            player.hp,
+            player.attackDamage
+        );
+        console.log(
+            "Boss %s has %s HP and %s AD",
+            bigBoss.name,
+            bigBoss.hp,
+            bigBoss.attackDamage
+        );
+
+        // Make sure the player has more than 0 HP.
+        require(player.hp > 0, "Error: character must have HP to attack boss.");
+
+        // Make sure the boss has more than 0 HP.
+        require(bigBoss.hp > 0, "Error: boss must have HP to attack boss.");
+
+        // Allow player to attack boss.
+        if (bigBoss.hp < player.attackDamage) {
+            bigBoss.hp = 0;
+        } else {
+            bigBoss.hp = bigBoss.hp - player.attackDamage;
+        }
+
+        // Allow boss to attack player.
+        if (player.hp < bigBoss.attackDamage) {
+            player.hp = 0;
+        } else {
+            player.hp = player.hp - bigBoss.attackDamage;
+        }
+        emit AttackComplete(bigBoss.hp, player.hp);
+
+        // Console for ease.
+        console.log("Player attacked boss. New boss hp: %s", bigBoss.hp);
+        console.log("Boss attacked player. New player hp: %s\n", player.hp);
+    }
+
+    function checkIfUserHasNFT()
+        public
+        view
+        returns (CharacterAttributes memory)
+    {
+        // Get the tokenId of the user's character NFT
+        uint256 userNftTokenId = nftHolders[msg.sender];
+        // If the user has a tokenId in the map, return their character.
+        if (userNftTokenId > 0) {
+            return nftHolderAttributes[userNftTokenId];
+        }
+        // Else, return an empty character.
+        else {
+            CharacterAttributes memory emptyStruct;
+            return emptyStruct;
+        }
+    }
+
+    function getAllDefaultCharacters()
+        public
+        view
+        returns (CharacterAttributes[] memory)
+    {
+        return defaultCharacters;
+    }
+
+    function getBigBoss() public view returns (BigBoss memory) {
+        return bigBoss;
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,36 +1,45 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
   const gameContract = await gameContractFactory.deploy(
-    ["Leo", "Aang", "Pikachu"],       // Names
-    ["https://i.imgur.com/pKd5Sdk.png", // Images
+    ["Leo", "Aang", "Pikachu"],
+    ["https://i.imgur.com/pKd5Sdk.png",
       "https://i.imgur.com/xVu4vFL.png",
-      "https://i.imgur.com/WMB6g9u.png"],
-    [100, 200, 300],                    // HP values
-    [100, 50, 25]                       // Attack damage values
+      "https://i.imgur.com/u7T87A6.png"],
+    [100, 200, 300],
+    [100, 50, 25],
+    "Elon Musk", // Boss name
+    "https://i.imgur.com/AksR0tt.png", // Boss image
+    10000, // Boss hp
+    50 // Boss attack damage
   );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 
-  let txn;
 
-  txn = await gameContract.mintCharacterNFT(0);
-  await txn.wait();
-  console.log('Minted NFT #1');
+  // let txn;
+  // txn = await gameContract.mintCharacterNFT(0);
+  // await txn.wait();
+  // console.log("Minted NFT #1");
 
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  console.log('Minted NFT #2');
+  // txn = await gameContract.mintCharacterNFT(1);
+  // await txn.wait();
+  // console.log("Minted NFT #2");
 
-  txn = await gameContract.mintCharacterNFT(2);
-  await txn.wait();
-  console.log('Minted NFT #3');
+  // txn = await gameContract.mintCharacterNFT(1);
+  // await txn.wait();
 
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  console.log('Minted NFT #4');
+  // // txn = await gameContract.mintCharacterNFT(1);
+  // // await txn.wait();
+  // // console.log("Minted NFT #4");
 
-  console.log("Done deploying and minting!!");
-}
+  // txn = await gameContract.attackBoss();
+  // await txn.wait();
+
+  // txn = await gameContract.attackBoss();
+  // await txn.wait();
+
+  // console.log("done")
+};
 
 const runMain = async () => {
   try {
@@ -40,6 +49,6 @@ const runMain = async () => {
     console.log(error);
     process.exit(1);
   }
-}
+};
 
 runMain();
